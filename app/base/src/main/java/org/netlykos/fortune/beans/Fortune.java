948c7f94a87fe0ce4dafac96d42529eb46964cb0
Adi B <adi.banga@fedex.com>
2022-06-08 17:05:18 +0100

    Updated to Java 17, Spring 2.7

--- app/base/src/main/java/org/netlykos/fortune/beans/Fortune.java
+++ app/base/src/main/java/org/netlykos/fortune/beans/Fortune.java
@@ -2,4 +2,9 @@ package org.netlykos.fortune.beans;
 
 import java.util.List;
 
+/**
+ * A record class that represents a fortune cookie.
+ *
+ * @author netlykos
+ */
 public record Fortune(String category, Integer number, List<String> lines) { }
