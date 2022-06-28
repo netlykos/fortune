ae5d2e0db9cc0a3b2924b093c4dbb98cb1e6fb24
Adi B <adi.banga@fedex.com>
2022-06-28 11:49:17 +0100

    fixed build for ng build, changed project name to ng-web-app, upgraded to ng 14

--- frontend/ng-web-app/karma.conf.js
+++ frontend/ng-web-app/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/web-app'),
+      dir: require('path').join(__dirname, './coverage/ng-web-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
